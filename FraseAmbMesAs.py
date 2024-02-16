50889733e9fe0c4f108612bbc34c309e8a3b9130
Ángel Borrás <angeloki1963@gmail.com>
2024-02-16 09:55:24 +0100

    Programa finalizado

--- FraseAmbMesAs.py
+++ FraseAmbMesAs.py
@@ -19,11 +19,22 @@ def contabilizador(frase):
     return(count)
 
 
-while True:
-    fraseInicial = cogerylower()
-    if fraseInicial == "fi":
-        break
-    else:
-     totalA = contabilizador(fraseInicial)
-     print("La frase: " + str(fraseInicial) + ". Es la frase con mas A's con: " + str(totalA) + " A's")
-            
+for i in range(1):
+    fraseInicial = "o"
+    fraselarga = fraseInicial
+    while True:
+        fraseInicial = cogerylower()
+        if fraseInicial == "fi":
+            break
+        else:
+        
+         totalA = contabilizador(fraseInicial)
+         
+         if totalA >= contabilizador(fraselarga):
+            fraselarga = fraseInicial
+         else:
+            pass
+         print("La frase: " + str(fraselarga) + ". Es la frase con mas A's con: " + str(contabilizador(fraselarga)) + " A's")
+         print(fraselarga)
+         
+                
