2e26f3f653a9fe0ce1dd0a76b91f045d9226da98
Ángel Borrás <angeloki1963@gmail.com>
2024-02-16 09:32:15 +0100

    Realizando prueba para la finalizacion y continuacion del programa

--- FraseAmbMesAs.py
+++ FraseAmbMesAs.py
@@ -2,6 +2,23 @@
 Primero creamos el def, luego lo agregaremos al bucle del programa
 """
 
-def parafrase():
+def cogerylower():
     frase = str(input("Ingrese una frase > "))
+    frase = frase.lower()
+    return(frase)
     
+
+def comparar()
+    fraseInicial = cogerylower()
+    if fraseInicial == "fi":
+        fraseFinal = "fi"
+    else:
+        fraseFinal = "El programa sigue"
+    return(fraseFinal)
+
+while True:
+    fraseFinal = comparar()
+    if fraseFinal == "fi":
+        break
+    else:
+        print(fraseFinal)
